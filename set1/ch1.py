ac1df616fe24b0c35b0f44779921c1fa96a81ea1
fjgl <fjluartes@gmail.com>
2020-06-02 11:16:31 +0100

    Edits ch1

--- set1/ch1.py
+++ set1/ch1.py
@@ -1,8 +1,18 @@
 # Set 1 Challenge 1
-# hex to base64
+# Convert hex to base64
+# hex string: 49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d 
+# base64: SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t 
+from binascii import hexlify, unhexlify
+from base64 import b64encode, b64decode
 
 def main():
-
+    hexstr = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+    b64str = b64encode(unhexlify(hexstr))
+    # b'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t'
+    b64dec = b64decode(b64str)
+    # b"I'm killing your brain like a poisonous mushroom"
+    print(b64str)
+    print(b64dec)
 
 if __name__ == "__main__":
     main()
---
+++ set1/ch2.py
@@ -0,0 +1,10 @@
+# Set 1 Challenge 2
+# 
+# 
+
+
+def main():
+
+
+if __name__ == "__main__":
+    main()
